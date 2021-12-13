843e09d9d4cda4d259bb75f0d2d044108858b39b
dayan27 <dayanyenesew@gmail.com>
2021-12-13 14:06:21 +0300

    tvet student fee view

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\DegreeStudentFeeController;
 use App\Http\Controllers\Registrar\AddressController;
 use App\Http\Controllers\Registrar\DegreeStudentController;
 use App\Http\Controllers\Registrar\TvetStudentController;
-
+use App\Http\Controllers\TvetStudentFeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -57,6 +57,7 @@ Route::apiResource('/degree_students',DegreeStudentController::class);
 Route::apiResource('/tvet_students',TvetStudentController::class);
 Route::apiResource('/address',AddressController::class);
 Route::get('/get_registrars',[EmployeeController::class,'getRegistrars']);
+Route::apiResource('/tvet_students_fees',TvetStudentFeeController::class);
 
 Route::apiResource('/degree_student_fees',DegreeStudentFeeController::class);
 
